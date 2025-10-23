51877f23513bc005742ac7f5cd93e574bba9d94a
AlexNikolov2 <aletoooking00@gmail.com>
2025-10-23 12:19:57 +0300

    added function

--- lab-1/ex2.cpp
+++ lab-1/ex2.cpp
@@ -14,6 +14,7 @@ private:
 public:
     timeclock();
     void display(const char *format = "24-hour");
+    void formattedDisplay(const char *format);
     ~timeclock()
     {
         cout << "\nDestructing object timeclock!\n";
@@ -43,18 +44,41 @@ void timeclock::display(const char *format)
     }
 }
 
+void timeclock::formattedDisplay(const char *format)
+{
+    if (format == "AM")
+    {
+        int displayHours = hours % 12;
+        displayHours = (displayHours == 0) ? 12 : displayHours;
+        cout << "Time is: " << displayHours << " hours, " << minutes << " minutes, " << seconds << " seconds AM.\n";
+    }
+    else if (format == "PM")
+    {
+        int displayHours = hours % 12;
+        displayHours = (displayHours == 0) ? 12 : displayHours;
+        cout << "Time is: " << displayHours << " hours, " << minutes << " minutes, " << seconds << " seconds PM.\n";
+    }
+}
+
 int main()
 {
 
     timeclock t1;
     t1.display();
+    t1.formattedDisplay("AM");
+    t1.formattedDisplay("PM");
 
     timeclock t2;
     t2.display();
+    t2.formattedDisplay("AM");
+    t2.formattedDisplay("PM");
 
     timeclock *ptr;
     ptr = new timeclock;
     ptr->display();
+    ptr->formattedDisplay("AM");
+    ptr->formattedDisplay("PM");
+    delete ptr;
 
     return 0;
 }
