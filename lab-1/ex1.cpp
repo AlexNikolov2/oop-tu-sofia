db7247ea972740253defac2c09918ea2e19eff04
AlexNikolov2 <aletoooking00@gmail.com>
2025-10-23 11:36:12 +0300

    nice fix

--- lab-1/ex1.cpp
+++ lab-1/ex1.cpp
@@ -22,6 +22,8 @@ triangle::triangle()
     {
         cout << "\n\nEnter three values for the sides of triangle:\n";
         cin >> a >> b >> c;
+        if (!((a > 0) && (b > 0) && (c > 0) && ((a + b > c) && ((a + c) > b) && ((b + c) > a))))
+            cout << "\nInvalid sides! Try again.";
     } while (!((a > 0) && (b > 0) && (c > 0) && ((a + b > c) && ((a + c) > b) && ((b + c) > a))));
 }
 double triangle::perimeter()
