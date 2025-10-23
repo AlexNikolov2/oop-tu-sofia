a3dbad337f6134efd75e91188b2dba20a0d43db1
AlexNikolov2 <aletoooking00@gmail.com>
2025-10-23 12:11:30 +0300

    making timeclock with time display

---
+++ lab-1/ex2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+class timeclock
+{
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
+public:
+    timeclock();
+    void display(const char *format = "24-hour");
+    ~timeclock()
+    {
+        cout << "\nDestructing object timeclock!\n";
+    }
+};
+timeclock::timeclock()
+{
+    do
+    {
+        cout << "\n\nEnter time in hours, minutes and seconds:\n";
+        cin >> hours >> minutes >> seconds;
+        if (!((hours >= 0 && hours < 24) && (minutes >= 0 && minutes < 60) && (seconds >= 0 && seconds < 60)))
+            cout << "\nInvalid time! Try again.";
+    } while (!((hours >= 0 && hours < 24) && (minutes >= 0 && minutes < 60) && (seconds >= 0 && seconds < 60)));
+}
+void timeclock::display(const char *format)
+{
+    if (strcmp(format, "12-hour") == 0)
+    {
+        int displayHours = hours % 12;
+        displayHours = (displayHours == 0) ? 12 : displayHours;
+        cout << "Time is: " << displayHours << " hours, " << minutes << " minutes, " << seconds << " seconds (12-hour format).\n";
+    }
+    else
+    {
+        cout << "Time is: " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds (24-hour format).\n";
+    }
+}
+
+int main()
+{
+
+    timeclock t1;
+    t1.display();
+
+    timeclock t2;
+    t2.display();
+
+    timeclock *ptr;
+    ptr = new timeclock;
+    ptr->display();
+    delete ptr;
+
+    return 0;
+}
