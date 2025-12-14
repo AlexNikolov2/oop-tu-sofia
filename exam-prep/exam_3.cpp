f6a492ecf23e2ecb95b8e4b876a4e5a1434aee17
AlexNikolov2 <aletoooking00@gmail.com>
2025-12-15 00:06:57 +0200

    fix

--- exam-prep/exam_3.cpp
+++ exam-prep/exam_3.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 class CircleArea
 {
-private:
+protected:
     double radius;
 
 public:
     CircleArea(double r) : radius(r) {}
+    virtual ~CircleArea() = default;
+
+    void setRadius(double r) { radius = r; }
     double getRadius() const { return radius; }
+
+    virtual double getArea() const = 0;
 };
 
 class Circle : public CircleArea
@@ -18,6 +24,7 @@ public:
     Circle(double r) : CircleArea(r) {}
     virtual ~Circle() {}
     double getRadius() const { return CircleArea::getRadius(); }
+    double getArea() const override { return M_PI * getRadius() * getRadius(); }
 };
 
 int main()
@@ -36,10 +43,10 @@ int main()
         circles[i] = new Circle(r);
     }
 
-    cout << "\nYou have created the following circles:\n";
+    cout << "\nYou have created the following circles with following area:\n";
     for (int i = 0; i < n; ++i)
     {
-        cout << "Circle " << (i + 1) << " with radius: " << circles[i]->getRadius() << "\n";
+        cout << "Circle " << (i + 1) << " with radius: " << circles[i]->getRadius() << " and area: " << circles[i]->getArea() << "\n";
         delete circles[i];
     }
     delete[] circles;
